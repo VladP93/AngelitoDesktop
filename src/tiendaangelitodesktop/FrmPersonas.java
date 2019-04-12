4e515a69521e519edd2f7e9e8807f6d9fe3bd6ec
DELL <DELL@E7440>
2019-04-12 02:02:55 -0600

    FORMULARIO DE CLIENTES, PERSONAS Y PROVEEDORES OTRA VEZ

--- src/tiendaangelitodesktop/FrmPersonas.java
+++ src/tiendaangelitodesktop/FrmPersonas.java
@@ -328,7 +328,7 @@ public void llenarTabla()
 
         
         
-        
+            
 
 }
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
