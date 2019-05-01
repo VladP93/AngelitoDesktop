77a6f32b8ca221c9eea116dc59ba15c2800954b9
Arnold <Arnold@DESKTOP-9KKR7U3>
2019-04-30 20:11:35 -0600

    Avance en Formulario Administrar productos y editar productos

--- src/dao/Producto.java
+++ src/dao/Producto.java
@@ -65,4 +65,41 @@ public class Producto {
     public ResultSet obtenerCodigos(String codigo){
         return cnx.getValores("SELECT COUNT(*) prod_idProducto FROM Producto WHERE prod_idProducto='"+codigo+"'");
     }
+    
+    
+    //A Partir de aquí
+    
+      
+    public ResultSet contarProductosConVentas(String codigo){
+        return cnx.getValores("SELECT COUNT(*) FROM Producto INNER JOIN detallefactura "
+                + "ON prod_idProducto = dfa_idProducto "
+                + "WHERE prod_idProducto='"+codigo+"'");
+    }
+    
+    public ResultSet contarProductosConCompras(String codigo){
+        return cnx.getValores("SELECT COUNT(*) FROM Producto INNER JOIN lote "
+                + "ON prod_idProducto = lot_idProducto "
+                + "WHERE prod_idProducto='"+codigo+"'");
+    }
+    
+    
+    public ResultSet mostrarNombres(String nombre){
+        return cnx.getValores("SELECT prod_nombre FROM producto WHERE prod_nombre='"+nombre+"'");
+    }
+    
+    public ResultSet obtenerIdProducto(String nombre){
+        return cnx.getValores("SELECT prod_idProducto FROM Producto WHERE prod_nombre='"+nombre+"'");
+    }
+    
+    public ResultSet filtrarProductos(String filtro) {
+                
+        return cnx.getValores("SELECT prod_nombre,prod_precio,prod_precioMayoreo,prod_iva,"
+                + "prod_cantidadMayoreo,prod_cantidad,prod_cantidadMinAlerta,cat_categoria,"
+                + "prod_descripcion FROM producto INNER JOIN categoria "
+                + "ON prod_idCategoria = cat_idCategoria "
+                + "WHERE prod_nombre LIKE '%"+filtro+"%'");
+    
+    }
+
+    
 }
